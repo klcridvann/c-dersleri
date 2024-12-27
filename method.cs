08df483257393d6a0a7abefa7b337e4cbaf54e71
klcridvann <ridvankilic@ogr.bandirma.edu.tr>
2024-12-27 04:35:35 +0300

    method.cs

---
+++ method.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //parametresiz geriye değer döndürmeyen metodlar
+            selamver();
+            adiniyazdir();
+           
+        }
+        static void selamver()
+        {
+            Console.WriteLine("selamlar:)");
+                 
+        }
+        static void adiniyazdir()
+        {
+            Console.WriteLine("Rıdvan Kılıç");
+         
+        }
+       
+    }
+}
